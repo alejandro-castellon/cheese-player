4200b07a228fee9f0974f7f842379f9a4b0593a3
Alejandro Castellon <alejandrocastellonfer@gmail.com>
2024-06-14 22:51:33 -0400

    rename navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/providers/theme-provider";
-import Navbar from "@/components/ui/Navbar";
+import Navbar from "@/components/ui/navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
